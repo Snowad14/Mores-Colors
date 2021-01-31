0bb9ffdd490e147c43b77e6898e8a957d9a8968b
Snowad14 <67842535+Snowad14@users.noreply.github.com>
2021-01-31 19:55:19 +0100

    Add files via upload

---
+++ MoreColors/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("MoreColors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoreColors")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
